7fba251deb9f7e23d0be0b4b8c5ec4e3c85f2360
daveroke <32863753+daveroke@users.noreply.github.com>
2017-10-23 21:53:38 +0200

    bullshit

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,18 @@ public class Main {
 
 
     public static void main(String[] args)  {
-        //List<Stuff> stuffs = new ArrayList<Stuff>(); //elemek listája
         Scanner scan = new Scanner(System.in);
 
-        int packwidth = scan.nextInt(); //poggyász szélessége
         int packheigth = scan.nextInt(); //poggyász magassága
+        int packwidth = scan.nextInt(); //poggyász szélessége
         int numOfStuff = scan.nextInt(); //elemek száma
-        int[] stuffSizes = new int[numOfStuff]; //elemek méretének// listája
         Stuff[] stuffs = new Stuff[numOfStuff];
 
         //elemek méreteinek beolvasása
         for (int i = 0; i < numOfStuff; i++){
-            int stuffW = scan.nextInt();
             int stuffH = scan.nextInt();
+            int stuffW = scan.nextInt();
             stuffs[i] = new Stuff(stuffW, stuffH, i); //elem init
-            stuffSizes[i] = stuffH*stuffW;
         }
         //Poggász init
         int[][] pack = new int[packwidth][packheigth];
@@ -31,38 +28,75 @@ public class Main {
                 pack[z][k] = 0;
             }
         }
-        int packedSize = packheigth*packwidth; //poggyász terület számítás
         
-        // Buborék rendezés az elemekre
-        for(int i = 0; i < (stuffSizes.length - 1); i++){
-            for(int f = 0; f < stuffSizes.length - i - 1; f++){
-                if(stuffSizes[f] < stuffSizes[f+1]){
-                    int temp = stuffSizes[f];
+        // Buborék rendezés az elemekre magasság szerint
+        for(int i = 0; i < (stuffs.length - 1); i++){
+            for(int f = 0; f < stuffs.length - i - 1; f++){
+                if(stuffs[f].getHeigth() < stuffs[f+1].getHeigth()){
                     Stuff stemp = stuffs[f];
-                    stuffSizes[f] = stuffSizes[f+1];
                     stuffs[f] = stuffs[f+1];
-                    stuffSizes[f+1] = temp;
                     stuffs[f+1] = stemp;
                 }
             }
         }
-
-        for(int i = 0; i < stuffSizes.length; i++){
-            if(packheigth >= stuffs[i].getHeigth() && packwidth >= stuffs[i].getWidth() && stuffSizes[i] <= packedSize){
+        //Buborék rendezés az elemekre szélesség szerint
+        for(int i = 0; i < (stuffs.length - 1); i++){
+            for(int f = 0; f < stuffs.length - i - 1; f++){
+                if(stuffs[f].getWidth() < stuffs[f+1].getWidth()){
+                    Stuff stemp = stuffs[f];
+                    stuffs[f] = stuffs[f+1];
+                    stuffs[f+1] = stemp;
+                }
+            }
+        }
+        int badheigth;
+        int badwidth;
+        boolean good = true;
+        for(int i = 0; i < stuffs.length; i++){
+            if(packheigth >= stuffs[i].getHeigth() && packwidth >= stuffs[i].getWidth() && good){
                 System.out.println("Belefér");
-                packedSize = packedSize - stuffSizes[i];
-                for (int a = 0; a < stuffs[i].getHeigth(); a++) {
-                    for (int b = 0; b < stuffs[i].getWidth(); b++) {
-                        pack[b][a] = stuffs[i].getStuffNumber();
+                for (int a = 0; a < packheigth; a++) {
+                    for (int b = 0; b < packwidth; b++) {
+                        if(pack[b][a] == 0){
+                            for (int c = 0; c < stuffs[i].getHeigth(); c++) {
+                                for (int d = 0; d < stuffs[i].getWidth(); d++) {
+                                    pack[d][c] = stuffs[i].getStuffNumber();
+                                }
+                            }
+                        }else{
+                            badheigth = a;
+                            badwidth = b;
+                            good = false;
+                            break;
+                        }
                     }
                 }
             }else{
                 stuffs[i].changeDir();
-                if(packheigth >= stuffs[i].getHeigth() && packwidth >= stuffs[i].getWidth() && stuffSizes[i] <= packedSize){
+                if(packheigth >= stuffs[i].getHeigth() && packwidth >= stuffs[i].getWidth() && good){
                     System.out.println("Belefér");
-                    packedSize = packedSize - stuffSizes[i];
-                }else {
-                    System.out.println("Nem fér bele");
+                    for (int a = 0; a < packheigth; a++) {
+                        for (int b = 0; b < packwidth; b++) {
+                            if(pack[b][a] == 0){
+                                for (int c = 0; c < stuffs[i].getHeigth(); c++) {
+                                    for (int d = 0; d < stuffs[i].getWidth(); d++) {
+                                        pack[d][c] = stuffs[i].getStuffNumber();
+                                    }
+                                }
+                            }else{
+                                badheigth = a;
+                                badwidth = b;
+                                good = false;
+                                break;
+                            }
+                        }
+                    }
+                }else{
+                    if(good == false){
+                        System.out.println("egymásra");
+                    }else{
+                        System.out.println("Nem fér bele");
+                    }
                 }
             }
         }
